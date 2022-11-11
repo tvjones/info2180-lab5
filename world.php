12f7f001fefb069dd429587d6dee18f4547f8dcd
Taye-Vaughn Jones <jonestaye@gmail.com>
2022-11-10 21:47:52 -0500

    Added the results to a table instead of an unordered list

--- world.php
+++ world.php
@@ -5,11 +5,10 @@ $password = 'password123';
 $dbname = 'world';
 
 
-if($_SERVER['REQUEST_METHOD'] === 'GET'){
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
   $country = $_GET["country"];
   $country = strip_tags($country);
   $country = strtolower($country);
- 
 }
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
@@ -17,14 +16,21 @@ $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-?>
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
 
-
-
-
-
+echo "<table>";
+  echo
+  "<tr>
+    <th>Country Name</th>
+    <th>Continent</th>
+    <th>Independence Year</th>
+    <th>Head of State</th>
+  </tr>";
+  foreach ($results as $row) : 
+    echo "<tr>
+    <td>".$row['name']."</td>
+      <td>".$row['continent']."</td>
+      <td>".$row['independence_year']."</td>
+      <td>".$row['head_of_state']."</td>
+    </tr>";
+  endforeach;
+echo "</table>";
